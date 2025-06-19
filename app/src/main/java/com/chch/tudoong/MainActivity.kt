8116b736aef56d9d10004c28417732b08a09d77a
Chaeyeon <qoreh940@gmail.com>
2025-06-19 16:39:36 +0900

    feat: Initialize Tudoong project and add TudoongTheme

---
+++ app/src/main/java/com/chch/tudoong/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.chch.tudoong
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.example.compose.TudoongTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TudoongTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)){
+                        Text("Tudoong")
+                    }
+                }
+            }
+        }
+    }
+}
