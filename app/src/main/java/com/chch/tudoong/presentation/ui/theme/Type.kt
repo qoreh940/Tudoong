2e798ef2da9cacfe9a7b8d1038020a1815322019
Chaeyeon <qoreh940@gmail.com>
2025-07-04 14:40:10 +0900

    chore: update app theme color palette

--- app/src/main/java/com/chch/tudoong/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/chch/tudoong/presentation/ui/theme/Type.kt
@@ -1,9 +1,13 @@
 package com.example.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
 import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.googlefonts.Font
 import com.chch.tudoong.R
 
 val provider = GoogleFont.Provider(
@@ -14,14 +18,14 @@ val provider = GoogleFont.Provider(
 
 val bodyFontFamily = FontFamily(
     Font(
-        googleFont = GoogleFont("Roboto Mono"),
+        googleFont = GoogleFont("Roboto"),
         fontProvider = provider,
     )
 )
 
 val displayFontFamily = FontFamily(
     Font(
-        googleFont = GoogleFont("Roboto Mono"),
+        googleFont = GoogleFont("Roboto"),
         fontProvider = provider,
     )
 )
