8032612c798be2ee626fd6d8b162325e4b898e99
Chaeyeon <qoreh940@gmail.com>
2025-07-07 16:49:50 +0900

    feat: add isMissed field to TodoItem, migrate database, and update database version

--- app/src/main/java/com/chch/tudoong/data/local/database/entities/TodoItem.kt
+++ app/src/main/java/com/chch/tudoong/data/local/database/entities/TodoItem.kt
@@ -2,6 +2,7 @@ package com.chch.tudoong.data.local.database.entities
 
 import androidx.room.*
 import com.chch.tudoong.domain.model.TodoType
+import com.chch.tudoong.presentation.ui.component.TdCheckboxState
 import java.util.*
 
 @Entity(tableName = "todo_items")
@@ -9,6 +10,14 @@ data class TodoItem(
     @PrimaryKey val id: String = UUID.randomUUID().toString(),
     val text: String,
     val isCompleted: Boolean = false,
+    val isMissed: Boolean = false, // database version 2 에 추가됨.
     val type: TodoType, // TODAY, YESTERDAY
     val createdAt: Long = System.currentTimeMillis()
-)
+){
+    val checkboxState: TdCheckboxState
+        get() = when {
+            isMissed -> TdCheckboxState.MISSED
+            isCompleted -> TdCheckboxState.DONE
+            else -> TdCheckboxState.NONE
+        }
+}
