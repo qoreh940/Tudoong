b01fa259d9f2ba4b5cd7934de7123d0fa8877f6c
Chaeyeon <qoreh940@gmail.com>
2025-07-16 15:31:25 +0900

    feat: Add checkbox state display to todo items in widget

--- app/src/main/java/com/chch/tudoong/widget/TudoongWidgetContent.kt
+++ app/src/main/java/com/chch/tudoong/widget/TudoongWidgetContent.kt
@@ -6,11 +6,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceModifier
+import androidx.glance.Image
+import androidx.glance.ImageProvider
+import androidx.glance.appwidget.enabled
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.lazy.items
 import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
+import androidx.glance.layout.size
+import androidx.glance.layout.width
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
@@ -20,43 +31,83 @@ import com.chch.tudoong.R
 @SuppressLint("RestrictedApi")
 @Composable
 fun TudoongWidgetContent(
-    todos : List<TodoWidgetItem>,
-    todayDate: String
-){
-    LazyColumn(
-        modifier = GlanceModifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(16.dp)
+    todos: List<TodoWidgetItem>,
+    todayDate: String,
+) {
+
+    Column(
+        modifier = GlanceModifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        item {
-            Text(
-                text = todayDate,
-                style = TextStyle(
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
-                ),
-                modifier = GlanceModifier.padding(bottom = 8.dp)
-            )
-        }
+        Text(
+            text = todayDate,
+            style = TextStyle(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = GlanceModifier
+                .fillMaxWidth()
+                .background(Color.LightGray)
+                .padding(start = 15.dp, top = 5.dp, bottom = 5.dp)
+        )
+        LazyColumn(
+            modifier = GlanceModifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(10.dp)
+        ) {
 
-        items(todos) {item ->
-            Text(
-                text = item.text
-            )
-        }
+            items(todos) { item ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    val bgRes = if (item.isMissed) R.drawable.widget_checkbox_border_missed
+                    else if (item.isCompleted) R.drawable.widget_checkbox_border_complete
+                    else R.drawable.widget_checkbox_border
+                    Box(
+                        modifier = GlanceModifier
+                            .enabled(item.isMissed)
+                            .background(ImageProvider(bgRes))
+                            .size(18.dp)
+                    ) {
+                        val imageRes = if (item.isMissed) R.drawable.ic_remove_24
+                        else if (item.isCompleted) R.drawable.ic_check_24
+                        else null
+                        imageRes?.let {
+                            Image(
+                                provider = ImageProvider(imageRes),
+                                contentDescription = "checkbox icon"
+                            )
+                        }
+                    }
 
-        if(todos.isEmpty()){
-            item {
-                Text(
-                    text = "There are no tasks for today.",
-                    style = TextStyle(
-                        fontSize = 14.sp,
-                        color = ColorProvider(R.color.gray)
-                    ),
-                    modifier = GlanceModifier.padding(top = 16.dp)
-                )
+                    Spacer(GlanceModifier.width(3.dp))
+
+
+                    Text(
+                        text = item.text,
+                        maxLines = 1,
+                        style = TextStyle(
+                            color = ColorProvider(if (item.isMissed) R.color.on_surface_variant_alpha60 else R.color.primary)
+                        )
+                    )
+                }
+            }
+
+            if (todos.isEmpty()) {
+                item {
+                    Text(
+                        text = "There are no tasks for today.",
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            color = ColorProvider(R.color.gray)
+                        ),
+                        modifier = GlanceModifier.padding(top = 16.dp)
+                    )
+                }
             }
         }
     }
+
+
 }
